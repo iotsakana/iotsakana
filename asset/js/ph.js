8179e9f4e14eec191af84247d3a40abe582c18cd
ardirs <ardi.rs@gmail.com>
2019-11-28 11:05:08 +0700

    update final

--- asset/js/ph.js
+++ asset/js/ph.js
@@ -1,13 +1,14 @@
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-// 5dcd37f56b01563ad3f47a87
+// 
 var url_string = window.location.href //
 var url = new URL(url_string);
-var id = url.searchParams.get("id");
+var id = url.searchParams.get("telegram");
 if (id == undefined) {
-    id = '5dcd37f56b01563ad3f47a87';
+    id = '-374817875';
 }
+
 var data = {
     suhu: 0,
     gas: 0,
@@ -19,31 +20,15 @@ var data = {
     relay1: false,
     relay2: false,
     relay3: false,
-    interval: 60
+    interval_name: "1 Jam"
 };
 
 var datenow = new Date();
 var datalast = new Date();
-axios.get('http://gradien.co:7777/api/users/' + id + '/data')
+
+axios.get('https://gradien.co:7777/api/telegram/' + id + "")
     .then((response) => {
-        if (response.data.data.mq == false) {
-            temp_gas = 0;
-        } else {
-            temp_gas = 1;
-        }
-        data.nama = response.data.data.nama;
-        data.relay_name = response.data.data.relay_name;
-        data.relay1 = response.data.data.relay1;
-        data.relay2 = response.data.data.relay2;
-        data.relay3 = response.data.data.relay3;
-        data.interval = response.data.data.interval;
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-axios.get('http://gradien.co:7777/api/records/' + id + '/last')
-    .then((response) => {
-        if (response.data.data.mq == false) {
+        if (response.data.data.gas == false) {
             temp_gas = 0;
         } else {
             temp_gas = 1;
@@ -55,52 +40,48 @@ axios.get('http://gradien.co:7777/api/records/' + id + '/last')
         data.ph = response.data.data.ph;
         data.date = new Date(response.data.data.created_at).toLocaleDateString('id-ID', options2);
         data.date_str = new Date(response.data.data.created_at).toLocaleDateString('id-ID', options);
+        data.nama = response.data.data.nama;
+        data.relay_name = response.data.data.relay_name;
+        data.relay1 = response.data.data.relay_1;
+        data.relay2 = response.data.data.relay_2;
+        data.relay3 = response.data.data.relay_3;
+        data.interval_name = response.data.data.interval_name;
+        if (response.data.data.msg != null) {
+            clearInterval(interval);
+            document.getElementById('app').innerHTML = ' <div class="pt-4 pl-2 pr-2 pb-4"></div><div class="col-12"><div class="pt-4 pr-4 pl-4 text-center"><div class="alert alert-danger alert-dismissible fade show" role="alert"><strong>Gagal Menyambung Ke Server</strong></div></div></div>';
+        }
     })
     .catch((error) => {
-        console.log(error.response);
-        document.getElementById('app').innerHTML = '<div class="col-12"><div class="pt-4 pr-4 pl-4 text-center"><div class="alert alert-danger alert-dismissible fade show" role="alert"><strong>Gagal Menyambung Ke Server</strong></div></div></div>';
+        document.getElementById('app').innerHTML = ' <div class="pt-4 pl-2 pr-2 pb-4"></div><div class="col-12"><div class="pt-4 pr-4 pl-4 text-center"><div class="alert alert-danger alert-dismissible fade show" role="alert"><strong>Gagal Menyambung Ke Server</strong></div></div></div>';
     });
 
-sleep(2000);
-
 function getData() {
-    axios.get('http://gradien.co:7777/api/records/' + id + '/last')
+    axios.get('https://gradien.co:7777/api/telegram/' + id + "")
         .then((response) => {
-            if (response.data.data.mq == false) {
+            if (response.data.data.gas == false) {
                 temp_gas = 0;
             } else {
                 temp_gas = 1;
             }
             const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'long' };
             const options2 = { day: 'numeric', month: 'long', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
-
             data.suhu = response.data.data.suhu;
             data.gas = temp_gas;
             data.ph = response.data.data.ph;
             data.date = new Date(response.data.data.created_at).toLocaleDateString('id-ID', options2);
             data.date_str = new Date(response.data.data.created_at).toLocaleDateString('id-ID', options);
-        })
-        .catch((error) => {
-            console.log(error.response);
-            document.getElementById('app').innerHTML = '<div class="col-12"><div class="pt-4 pr-4 pl-4 text-center"><div class="alert alert-danger alert-dismissible fade show" role="alert"><strong>Gagal Menyambung Ke Server</strong></div></div></div>';
-        });
-
-    axios.get('http://gradien.co:7777/api/users/' + id + '/data')
-        .then((response) => {
             data.nama = response.data.data.nama;
             data.relay_name = response.data.data.relay_name;
             data.relay1 = response.data.data.relay_1;
             data.relay2 = response.data.data.relay_2;
             data.relay3 = response.data.data.relay_3;
-            data.interval = response.data.data.interval;
+            data.interval_name = response.data.data.interval_name;
         })
         .catch((error) => {
-            console.log(error.response);
+            document.getElementById('app').innerHTML = ' <div class="pt-4 pl-2 pr-2 pb-4"></div><div class="col-12"><div class="pt-4 pr-4 pl-4 text-center"><div class="alert alert-danger alert-dismissible fade show" role="alert"><strong>Gagal Menyambung Ke Server</strong></div></div></div>';
         });
-
     return data;
 }
-
 
 Plotly.plot('ph', [{
     y: [data.ph],
@@ -154,9 +135,9 @@ var cnt = 0;
 var relay_1 = './asset/img/off.jpg';
 var relay_2 = './asset/img/off.jpg';
 var relay_3 = './asset/img/off.jpg';
-setInterval(function() {
+
+var interval = setInterval(function() {
     data = getData();
-    console.log(data);
     document.getElementById("date-custome1").innerHTML = data.date_str;
     document.getElementById("date-custome2").innerHTML = data.date_str;
     document.getElementById("date-custome3").innerHTML = data.date_str;
@@ -171,18 +152,24 @@ setInterval(function() {
     relay_3 = './asset/img/off.jpg';
     if (data.relay1 == true) {
         relay_1 = './asset/img/on.jpg';
+    } else if (data.relay1 == false) {
+        relay_1 = './asset/img/off.jpg';
     }
     if (data.relay2 == true) {
         relay_2 = './asset/img/on.jpg';
+    } else if (data.relay2 == false) {
+        relay_1 = './asset/img/off.jpg';
     }
     if (data.relay3 == true) {
         relay_3 = './asset/img/on.jpg';
+    } else if (data.relay3 == false) {
+        relay_1 = './asset/img/off.jpg';
     }
     document.getElementById("relay1status").src = relay_1;
     document.getElementById("relay2status").src = relay_2;
     document.getElementById("relay3status").src = relay_3;
-
-    document.getElementById("nama").innerHTML = data.nama;
+    document.getElementById("intervalstatus").innerHTML = data.interval_name;
+    document.getElementById("nama").innerHTML = '<button class="btn btn-outline-success my-2 my-sm-0" type="submit" disabled>' + data.nama + '</button>';
     //=================================================
     // Hendeling When Data is Stack / Data not update;
     datanow = data.date;
